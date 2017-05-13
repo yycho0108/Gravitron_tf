ed9710683e5d4a0daa530ae34cc00febd3d3414c
yycho0108 <jchocholate@gmail.com>
2017-05-13 08:29:18 -0400

    initial commit

---
+++ gravitron.py
@@ -0,0 +1,168 @@
+import gym
+from gym import spaces
+
+import numpy as np
+import cv2
+from timer import Timer
+
+from config import *
+
+M = 0
+L = 1
+R = 2
+a_dir = {
+        M : 0,
+        L : -1,
+        R : 1,
+        }
+
+class Ball(object):
+    def __init__(self, i, j, d):
+        self.i = i
+        self.j = j
+        self.dir = d
+        self.warn = True
+        self.age = 0
+
+    def step(self):
+        if self.age > 5:
+            self.warn = False
+        if not self.warn:
+            self.j += self.dir * BALL_SPEED
+        self.age += 1
+
+    def render(self, frame):
+        if self.warn:
+            if self.dir > 0:
+                pt1 = (self.j, self.i - BALL_R)
+                pt2 = (self.j, self.i + BALL_R)
+                pt3 = (self.j+BALL_R, self.i)
+            else:
+                pt1 = (self.j, self.i - BALL_R)
+                pt2 = (self.j, self.i + BALL_R)
+                pt3 = (self.j-BALL_R, self.i)
+
+            #cv2.fillConvexPoly(frame, [pt1,pt2,pt3], 255)
+            cv2.fillPoly(frame, np.array([[pt1,pt2,pt3]]), 255)
+
+        else:
+            cv2.circle(frame, (self.j, self.i), BALL_R, 255, -1)
+
+def collisionCheck(cir, rect):
+    cy,cx,cr = cir
+    ry,rx,rh,rw = rect
+    dx = cx - max(rx, min(cx, rx + rw))
+    dy = cy - max(ry, min(cy, ry + rh))
+    return dx*dx + dy*dy < cr*cr
+
+class Gravitron(gym.Env):
+    metadata = {
+            'render.modes' : ['human']
+            }
+    def __init__(self, w=WIN_W, h=WIN_H):
+        self.w, self.h = w,h
+        self.action_space = spaces.Discrete(2) # left / right
+        low = np.zeros((self.h, self.w), dtype=np.uint8)
+        high = np.full((self.h, self.w), 255, dtype=np.uint8)
+        self.action_space = spaces.Discrete(3)
+        self.observation_space = spaces.Box(low, high)
+
+        self.reset()
+        self.drawing = False 
+
+    def _reset(self):
+        self.time = 0
+        self.frame = np.zeros((self.h, self.w), dtype=np.uint8)
+        self.balls = []
+        self.i, self.j = self.h/2, self.w/2
+        self.dir = 1
+        self.draw()
+        return self.state()
+    def _step(self, action):
+
+        if self.time % BALL_SPAWN_PERIOD == 0:
+            i = np.random.randint(6) * (BALL_R*2 + BALL_GAP) + BALL_R
+            j = np.random.choice((0,self.w-1))
+            self.balls.append(Ball(i,j,(-1 if j else 1)))
+
+
+        # MOVE CHAR 
+        self.i += self.dir * CHAR_SPEED_Y
+        self.j += a_dir[action] * CHAR_SPEED_X
+
+        if self.i >= self.h:
+            self.dir = -self.dir
+            self.i = self.h - 1
+        elif self.i <= 0:
+            self.dir = -self.dir
+            self.i = 0
+
+        self.j %= self.w
+
+        # MOVE BALLS
+        self.balls = filter(lambda b: (b.j>=0 and b.j<self.w), self.balls)
+
+        p1_x = self.j - CHAR_W/2
+        p2_x = self.j + CHAR_W/2
+        p1_y = self.i - CHAR_H/2
+        p2_y = self.i + CHAR_H/2
+        done = False
+        for b in self.balls:
+            b.step()
+            if collisionCheck((b.i,b.j,BALL_R),(p1_y, p1_x, CHAR_H, CHAR_W)):
+                done = True
+        # DRAW
+
+        self.draw()
+        self.time += 1
+
+        reward = -1 if done else 0.001#?
+        return self.state(), reward, done, {}
+
+    def _render(self, mode='human', close=False):
+        if close:
+            self.drawing = False
+            cv2.destroyAllWindows()
+            return
+        if not self.drawing:
+            self.drawing = True
+            cv2.namedWindow('Gravitron')
+            cv2.moveWindow('Gravitron', 100,50)
+        cv2.imshow('Gravitron', self.frame)
+
+    def draw(self):
+        self.frame.fill(0)
+        p1_x = self.j - CHAR_W/2
+        p2_x = self.j + CHAR_W/2
+        p1_y = self.i - CHAR_H/2
+        p2_y = self.i + CHAR_H/2
+        cv2.rectangle(self.frame, (p1_x,p1_y), (p2_x,p2_y), 255, -1)
+        for b in self.balls:
+            b.render(self.frame)
+    def state(self):
+        return np.expand_dims((self.frame == 255).astype(np.float32), axis=2)
+
+def main():
+    W,A,S,D = 119,97,115,100
+    env = Gravitron()
+    d = False
+    s0 = env.reset()
+    a = M
+    with Timer('run'):
+        while not d:
+            env.render()
+            k = cv2.waitKey(30)
+
+            if k == A:
+                a = L
+            elif k == D:
+                a = R
+            elif k == 255:
+                pass
+            else:
+                a = M
+            s, r, d, _ = env.step(a)
+        print env.time
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,168 @@
+#!/usr/bin/python
+
+import sys
+import numpy as np
+import tensorflow as tf
+
+from config import *
+from memory import Memory, MultiMemory
+from qnet import *
+from layers import *
+from gravitron import Gravitron
+
+## Load Parameters
+gamma = .99 #Discount factor.
+num_episodes = 10000 #Total number of episodes to train network for.
+test_episodes = 200 #Total number of episodes to train network for.
+tau = 0.001 #Amount to update target network at each step.
+batch_size = 128 #Size of training batch
+
+eps_start = 1 #Starting chance of random action
+eps_end = 0.05 #Final chance of random action
+annealing_steps = 200000 #How many steps of training to reduce startE to endE.
+eps_delta = (eps_end - eps_start) / annealing_steps
+
+pre_train_steps = 50000 #Number of steps us
+
+## Initialize Tensorflow
+tf.reset_default_graph()
+session = tf.Session()
+
+env = Gravitron()
+
+def train(net, target_net, memory, episodes):
+
+    eps = eps_start
+    step = 0
+    rewards = []
+
+    def train_once(eps, step):
+        s0 = env.reset()
+        net_reward = 0
+        d = False
+        while not d:
+            if np.random.rand(1) < eps or step < pre_train_steps:
+                a = env.action_space.sample()
+            else:
+                a = session.run(net.predict, feed_dict={net.inputs:[s0]})
+                a = a[0]
+
+            s1,r,d,_ = env.step(a)
+            memory.add({'s0':s0, 'a':a, 'r':r, 's1':s1, 'd':d})
+            if step > pre_train_steps:
+                if eps > eps_end:
+                    eps += eps_delta
+                if step % 5 == 0:
+                    input_batch = memory.sample(batch_size)
+                    _s0 = input_batch['s0'].astype(np.uint8)
+                    _a = input_batch['a']
+                    _r = input_batch['r']
+                    _s1 = input_batch['s1'].astype(np.uint8)
+                    _d = input_batch['d']
+
+                    # s1 = (4,32), --> (x,32)
+                    a_s1 = session.run(net.predict, feed_dict={net.inputs : _s1})
+                    q_s1 = session.run(target_net._Q, feed_dict={target_net.inputs : _s1})
+                    q = q_s1[range(batch_size), a_s1].reshape((batch_size,-1))
+                    target_q = _r + gamma * q * (1 - _d)
+                    _ = session.run(net.update, feed_dict = {net.inputs : _s0, net.Qn:target_q, net.actions:_a})
+                    session.run(copy_ops)
+            net_reward += r
+            s0 = s1
+            step += 1
+        return net_reward, eps, step
+
+    r_mean = 0
+    i = 0
+    
+    while i < episodes: 
+        net_reward, eps, step = train_once(eps, step)
+        rewards.append(net_reward)
+
+        if i % 10 == 0 and i > 0:
+            r_mean = np.mean(rewards[-100:])
+            print "Epoch : %d, Mean Reward: %f; Step : %d, Epsilon: %f" % (i, r_mean, step, eps)
+        i += 1
+
+    if raw_input('Train Until Convergence?\n').lower() == 'y':
+        while r_mean < 999:
+            net_reward, eps, step = train_once(eps, step)
+            rewards.append(net_reward)
+
+            if i % 10 == 0 and i > 0:
+                r_mean = np.mean(rewards[-100:])
+                print "Epoch : %d, Mean Reward: %f; Step : %d, Epsilon: %f" % (i, r_mean, step, eps)
+            i += 1
+
+
+    return rewards
+
+def test(net, episodes):
+    rewards = []
+
+    # test
+    for i in range(episodes):
+        s = env.reset()
+        d = False
+        net_reward = 0
+        while not d:
+            env.render()
+            a = session.run(net.predict, feed_dict={net.inputs:[s]})
+            a = a[0]
+            s,r,d,_ = env.step(a)
+            net_reward += r
+        rewards.append(net_reward)
+    return rewards
+
+def setup():
+    ## Start Environment
+    state_size = reduce(lambda x,y:x*y, env.observation_space.shape)
+
+    # initialize memory
+    memory = MultiMemory(pre_train_steps)
+    def create_network():
+        net = QNet((WIN_H,WIN_W,1))
+        net.append(ConvolutionLayer((3,3,1,4)))
+        net.append(ActivationLayer('relu'))
+        net.append(DenseLayer((WIN_H*WIN_W*4,256)))
+        net.append(ActivationLayer('relu'))
+        net.append(DenseLayer((256,3)))
+        #net.append(ActivationLayer('relu'))
+        net.setup()
+        return net
+
+
+    # get networks
+    with tf.variable_scope('net') as scope:
+        net = create_network()
+    with tf.variable_scope('target') as scope:
+        target_net = create_network()
+
+    return net, target_net, memory
+
+def main():
+    global copy_ops
+
+    net, target_net, memory = setup()
+    copy_ops = net.copyTo(target_net, tau)
+
+    # get this started...
+    session.run(tf.global_variables_initializer())
+    session.run(copy_ops)
+    saver = tf.train.Saver()
+
+    if len(sys.argv) > 1 and sys.argv[1].lower() == 'load':
+        saver.restore(session, '/tmp/model.ckpt')
+        print '[loaded]'
+    else:
+        train_rewards = train(net, target_net, memory, num_episodes)
+        np.savetxt('train.csv', train_rewards, delimiter=',', fmt='%f')
+        save_path = saver.save(session, '/tmp/model.ckpt')
+        print("Model saved in file: %s" % save_path) 
+
+    test_rewards = test(net, test_episodes)
+    np.savetxt('test.csv', test_rewards, delimiter=',', fmt='%f')
+
+if __name__ == "__main__":
+    main()
+    
